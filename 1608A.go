5a2c7d985e2856ada3869cf28498e2b7eeb49e5d
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-06-03 08:58:17 +0800

    Add files via upload

---
+++ 1608A.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t, n int
+	for fmt.Fscan(in, &t); t > 0; t-- {
+		fmt.Fscan(in, &n)
+		for i := 1; i <= n; i++ {
+			fmt.Fprint(out, i+1, " ")
+		}
+		fmt.Fprintln(out)
+	}
+}
