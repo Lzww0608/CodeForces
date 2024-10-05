5e4e98c46da7de1585b8884fa8e9ff1642e8005a
Lzww <63537700+Lzww0608@users.noreply.github.com>
2024-10-05 13:08:30 +0800

    Add files via upload

---
+++ 1200/1909B.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	for fmt.Fscan(in, &t); t > 0; t-- {
+		solve(in, out)
+	}
+}
+
+func solve(in *bufio.Reader, out *bufio.Writer) {
+	var n int
+	fmt.Fscan(in, &n)
+	a := make([]int, n)
+	for i := range a {
+		fmt.Fscan(in, &a[i])
+	}
+	k := 1
+	for {
+		for _, x := range a {
+			if x%k != a[0]%k {
+				fmt.Fprintln(out, k)
+				return
+			}
+		}
+		k <<= 1
+	}
+	return
+}
