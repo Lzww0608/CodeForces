ac0ec21826b210fdd660f3e0ef4fb23882757834
Lzww <63537700+Lzww0608@users.noreply.github.com>
2025-04-03 11:18:20 +0800

    Add files via upload

---
+++ 1300/1498B.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	var t int
+	for fmt.Fscan(in, &t); t > 0; t-- {
+		solve(in, out)
+	}
+}
+
+func solve(in *bufio.Reader, out *bufio.Writer) {
+	var n, w int
+	fmt.Fscan(in, &n, &w)
+	a := make([]int, n)
+	for i := range a {
+		fmt.Fscan(in, &a[i])
+	}
+
+	sort.Ints(a)
+	h := &hp{}
+	for i := n - 1; i >= 0; i-- {
+		x := a[i]
+		if h.Len() == 0 || x > (*h)[0] {
+			heap.Push(h, w-x)
+		} else {
+			cur := heap.Pop(h).(int)
+			cur -= x
+			heap.Push(h, cur)
+		}
+	}
+	fmt.Fprintln(out, h.Len())
+}
+
+type hp []int
+
+func (h hp) Len() int           { return len(h) }
+func (h hp) Less(i, j int) bool { return h[i] > h[j] }
+func (h hp) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+func (h *hp) Push(v interface{}) {
+	*h = append(*h, v.(int))
+}
+func (h *hp) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
